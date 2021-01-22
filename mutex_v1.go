768e78f6e25b2f80f1814e0c55df0a2edbed5dab
eswaribala <Parameswaribala@gmail.com>
2021-01-22 14:47:32 +0530

    Add files via upload

---
+++ mutex_v1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	// Balance: shared variable
+	balance := 0
+
+	// A single mutex to be used by both goroutines
+	var m sync.Mutex
+
+	// Function that accesses balance multiple times
+	counter := func() {
+		m.Lock()
+		for i := 0; i < 10000000; i++ {
+			balance = balance + 10
+		}
+		m.Unlock()
+	}
+
+	// Starting two goroutines
+	go counter()
+	go counter()
+
+	// Waiting and then printing balance
+	time.Sleep(time.Second)
+	fmt.Printf("Balance: %v", balance) // should be 200000000
+}
